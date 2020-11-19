8c5388a202f8a29b9fbdd9b8379726a3eaf5d0b3
jiushu <xpfwork@foxmail.com>
2020-11-19 10:26:20 +0800

    观察者模式

--- app/src/test/java/com/xpf/design/patterns/observer/ObserverTest.java
+++ app/src/test/java/com/xpf/design/patterns/observer/ObserverTest.java
@@ -7,6 +7,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * 观察者模式
+ * 模式定义：定义了对象之间的一对多依赖，让多个观察者同时监听某一个主题对象，
+ * 当这个对象发生改变时，它的所有依赖着都会收到通知并更新/触发技能
+ *
+ * 应用场景：当更改一个对象的状态可能需要更改其它对象，
+ * 并且实际的对象集事先未知或动态更改时，使用观察者模式
+ *
+ * 优点：
+ * 1.符合开闭原则：添加新的观察者的时候只需要实现observer接口，添加到主题对象的容器列表，
+ *   不需要修改已有观察者的代码
+ * 2.可以在运行时建立对象之间的关系：可以动态的添加新的观察者，
+ *   也可以以前的观察者移除掉。
+ */
+public class ObserverTest {
+    @Test
+    public void test() {
+        Subject subject = new Subject();
+        Task1 task1 = new Task1();
+        subject.addObserver(task1);
+        Task2 task2 = new Task2();
+        subject.addObserver(task2);
+
+        subject.notifyObserver("xxx");
+        System.out.println("=============");
+        subject.remove(task1);
+        subject.notifyObserver("yyy");
+    }
+
+}
+
 class Subject {
     //容器
     private List<Observer> container = new ArrayList<>();
